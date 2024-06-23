4898b7730c97cf0a243ce2252a6573e7b7cc9bcf
prakash2003pramanick <prakashpramanickjsr1717@gmail.com>
2024-06-24 03:58:07 +0530

    registration form added

--- controllers/forms/formController.js
+++ controllers/forms/formController.js
@@ -3,10 +3,14 @@ const { addForm }= require('./addForm');
 const { editForm} = require('./editForm');
 const { deleteForm }= require('./deleteForm');
 const { getAllForms } = require('./getForm');
+const { addSection } = require('./addSection');
+const { getSections } = require('./getSection');
 
 module.exports = {
     addForm,
     editForm,
     deleteForm,
     getAllForms,
+    addSection,
+    getSections
 };
