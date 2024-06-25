38fe1d0ac571af926cf1a076f6678a7eb52ac039
Vinayak Puranik <vinayakpuranik0707@gmail.com>
2024-06-25 21:29:12 +0530

    New feature forgot password have been added

--- controllers/auth/forgetPassword.js
+++ controllers/auth/forgetPassword.js
@@ -11,53 +11,12 @@ const { ApiError } = require('../../utils/ApiError');
 //@access          Public
 
 
-const verifyOtp = expressAsyncHandler(async (req, res, next) => {
-    /*
-    -> forgetPassword db me search for user email 
-    -> match the otp 
-    -> delete the instance 
-    
-    */
-   
-   const userEnteredOTP = req.body.otp;
-   const emailOfUser = req.body.email;
-   const user = await prisma.forgotPassword.findUnique({
-       where: {
-           email: emailOfUser
-        }
-    });
-    if (userEnteredOTP == user.otp) {
-        await prisma.forgotPassword.delete({
-            where: {
-                email: emailOfUser
-            }
-        });
-        res.json({message : "OTP is verified"})
-    } else {
-        res.json({message: "Entered OTP is invalid!!"});
-    }});
-    
-    const changePassword = expressAsyncHandler(async (req, res, next) => {
-        /*
-            -> take the new password from the user in the req
-            -> hash the password using bcrypt 
-            -> store the new password in the password field of the userdb
-            */
-    
-           const newPassword = req.body.newPassword;
-        const confirmNewPassword = req.body.confirmNewPassword;
-        const emailOfUser = req.body.email;
-        if(newPassword==confirmNewPassword){
-            const existingUser = await prisma.user.findUnique({
-                where: { emailOfUser }
-            });
-            existingUser.password = newPassword;
-            res.json({user:existingUser})
-        }
-        else{
-            res.json({message: "Confirmation password does not match the new password !!"})
-        }
-    })
+
+const otpGenerator=()=>{
+    const otpGenerator = require('otp-generator')
+    let generatedOTP = otpGenerator.generate(6, { upperCaseAlphabets: false, lowerCaseAlphabets:false, specialChars: false });
+    return generatedOTP;
+}
     
     const forgetPassword = expressAsyncHandler(async (req, res, next) => {
         /*
@@ -83,10 +42,9 @@ const verifyOtp = expressAsyncHandler(async (req, res, next) => {
                 const existingUser = await prisma.user.findUnique({
                     where: { email : emailOfUser }
                 });
-    
-                // let generatedOTP = Math.floor(Math.random()*999999)
-                const otpGenerator = require('otp-generator')
-                let generatedOTP = otpGenerator.generate(6, { upperCaseAlphabets: false, lowerCaseAlphabets:false, specialChars: false });
+
+                let generatedOTP = otpGenerator();
+
         
                 if (existingUser) {
                     const user = await prisma.forgotPassword.create({
@@ -95,14 +53,18 @@ const verifyOtp = expressAsyncHandler(async (req, res, next) => {
                             otp:generatedOTP
                         }
                     });
-                    res.json({user : existingUser,otp:generatedOTP,forgotData : user})
+                    res.json({otp:generatedOTP,message : "OTP will not work after 10mins"})
 
-                    // await prisma.forgotPassword.delete({
-                    //     where: {
-                    //         email: emailOfUser
-                    //     }
-                    // });
-                    // res.send("Deleted")
+                    const autoDelete = async()=>{
+                        await prisma.forgotPassword.delete({
+                            where : {email : emailOfUser}
+                        })
+                    }
+
+                    setTimeout(()=>{
+                        autoDelete();
+                    },600000);
+
                 }
                 else{
                     res.json({message:"User Not found"})
