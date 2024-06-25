94e1a0dcc55e86140fdd5579846b984450f7b6e9
Amaldeep <amaldeeppatra21@gmail.com>
2024-06-25 17:16:55 +0530

    nodemailer done

--- config/nodeMailer.js
+++ config/nodeMailer.js
@@ -1,1 +1,12 @@
 //config for nodeMailer
+const nodemailer = require('nodemailer');
+
+const mailTransporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'amaldeeppatra21@gmail.com',
+        pass: 'wblmwknwabjgxtpb'
+    }
+});
+
+module.exports = mailTransporter;
