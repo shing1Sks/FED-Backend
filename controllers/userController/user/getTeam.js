af8f7ab61fbc6f511b3a065cd4860da821dbc79a
prakash2003pramanick <prakashpramanickjsr1717@gmail.com>
2024-06-27 16:46:04 +0530

    fetch Team updated

--- controllers/userController/user/getTeam.js
+++ controllers/userController/user/getTeam.js
@@ -1,39 +1,33 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient, AccessTypes } = require('@prisma/client');
 const prisma = new PrismaClient();
 const expressAsyncHandler = require('express-async-handler');
 const { ApiError } = require('../../../utils/error/ApiError');
 
 //@description     Fetch Teams
 //@route           GET /api/user/team
-//@access          Admin
+//@access          Public
 const fetchTeam = expressAsyncHandler(async (req, res, next) => {
     try {
-        // Fetch users where member is not null
+        // Fetch users with specific access types
         const users = await prisma.user.findMany({
             where: {
-                member: {
-                    not: null
+                access: {
+                    notIn: [AccessTypes.USER, AccessTypes.ADMIN]
                 }
             },
-            select : {
+            select: {
                 name: true,
-                email:true,
-                "member": true,
-                password : false
+                access: true,
+                email: true,
+                extra: true
             }
         });
 
-        // Filter out users with empty member objects
-        const teams = users.filter(user => {
-            const member = user.member;
-            return member && Object.keys(member).length > 0;
-        });
-
-        if (teams.length === 0) {
+        if (users.length === 0) {
             return next(new ApiError(404, 'No teams found'));
         }
 
-        res.status(200).json({ success: true, data: teams });
+        res.status(200).json({ success: true, data: users });
     } catch (error) {
         console.error('Error fetching teams:', error);
         next(new ApiError(500, 'Error fetching teams', error));
