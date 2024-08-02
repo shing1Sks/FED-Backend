7fd61c8f267ce315213558965fc55c39e1422479
prakash2003pramanick <prakashpramanickjsr1717@gmail.com>
2024-08-03 00:19:46 +0530

    update

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type,Authorization, multipart/form-data');
-    res.setHeader('Access-Control-Allow-Credentials', false);
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 app.use((req, res, next) => {
