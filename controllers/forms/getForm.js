e3a5645b196b744ba9b92c682446ce976ac9d996
prakash2003pramanick <prakashpramanickjsr1717@gmail.com>
2024-08-03 15:43:37 +0530

    update

--- controllers/forms/getForm.js
+++ controllers/forms/getForm.js
@@ -9,22 +9,22 @@ const getAllForms = async (req, res, next) => {
     try {
         const forms = await prisma.form.findMany({});
 
-        const filteredForms = forms.reduce((acc, form) => {
-            // Filter out forms where isPublic is false
-            if (!form.info.isPublic) return acc;
+        // const filteredForms = forms.reduce((acc, form) => {
+        //     // Filter out forms where isPublic is false
+        //     if (!form.info.isPublic) return acc;
 
-            // Remove sections from forms where isEventPast is true
-            form.sections = !(form.info.isEventPast || form.info.isRegistrationClosed) ? form.sections : null;
+        //     // Remove sections from forms where isEventPast is true
+        //     form.sections = !(form.info.isEventPast || form.info.isRegistrationClosed) ? form.sections : null;
 
-            // Add the form to the accumulator
-            acc.push(form);
-            return acc;
-        }, []);
+        //     // Add the form to the accumulator
+        //     acc.push(form);
+        //     return acc;
+        // }, []);
 
         res.status(200).json({
             success: true,
             message: 'All forms fetched successfully',
-            forms: filteredForms,
+            events : forms
         });
     } catch (error) {
         console.error('Error while getting forms:', error);
