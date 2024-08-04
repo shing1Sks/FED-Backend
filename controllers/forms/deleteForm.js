40e5e6262db591ff976de79c76c708df23c38abd
prakash2003pramanick <prakashpramanickjsr1717@gmail.com>
2024-08-04 23:52:34 +0530

    update

--- controllers/forms/deleteForm.js
+++ controllers/forms/deleteForm.js
@@ -1,11 +1,10 @@
-// controllers/form/deleteForm.js
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const { ApiError } = require('../../utils/error/ApiError');
 const deleteImage = require('../../utils/image/deleteImage');
 
 //@description     Delete Form
-//@route           delete /api/form/deleteForm/:id
+//@route           DELETE /api/form/deleteForm/:id
 //@access          Admins
 const deleteForm = async (req, res, next) => {
     console.log("deleteForm");
@@ -16,22 +15,31 @@ const deleteForm = async (req, res, next) => {
             where: { id: formId },
         });
 
-        // Delete image from cloudinary
-        if(deletedForm && deletedForm.info && deletedForm.info.eventImg){
-            deleteImage(deletedForm.info.eventImg, 'FormImages')
-        }
+        // Delete image from cloudinary using promise
+        const imageDeletePromise = deletedForm && deletedForm.info && deletedForm.info.eventImg
+            ? deleteImage(deletedForm.info.eventImg, 'FormImages')
+            : Promise.resolve();
 
         // Delete all registrations
-        if(req.body.deleteRegistations){
-            await prisma.formRegistration.delete({
-                where : { formId : formId }
-            })
+        if (req.body.deleteRegistrations) {
+            await prisma.formRegistration.deleteMany({
+                where: { formId: formId }
+            });
         }
 
+        // Handle the image deletion promise
+        imageDeletePromise
+            .then((result) => {
+                console.log('Image deleted successfully:', result);
+            })
+            .catch((error) => {
+                console.error('Error in deleting image:', error);
+            });
         res.status(200).json({
             success: true,
             message: 'Form deleted successfully',
         });
+
     } catch (error) {
         console.error('Error in deleting form:', error);
         return next(new ApiError(500, 'Error in deleting form', error));
--- utils/image/uploadImage.js
+++ utils/image/uploadImage.js
@@ -1,18 +1,18 @@
 // uploadImage.js
 const cloudinary = require('../../config/cloudinary');
 const fs = require('fs');
-const ApiError = require('../../utils/error/ApiError');
+const { ApiError } = require('../error/ApiError');
 
 const uploadImage = async (filePath, folder = 'MemberImages', height = 150, width = 150, crop = 'limit') => {
     try {
         console.log("Uploading file:", filePath);
-        const options = { 
+        const options = {
             folder: folder,
             height: height,
             width: width,
             crop: crop
         };
-        console.log("Options are",options);
+        console.log("Options are", options);
         const result = await cloudinary.uploader.upload(filePath, options);
         fs.unlinkSync(filePath); // Remove file from server after upload to Cloudinary
         console.log("Image uploaded successfully:", result);
