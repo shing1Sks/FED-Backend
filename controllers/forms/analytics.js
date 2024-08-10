f74a9c572ed3a780996402c836092690b7108144
prakash2003pramanick <prakashpramanickjsr1717@gmail.com>
2024-08-10 23:25:16 +0530

    form analytics route protected

--- controllers/forms/analytics.js
+++ controllers/forms/analytics.js
@@ -3,26 +3,42 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const { ApiError } = require('../../utils/error/ApiError');
 const expressAsyncHandler = require('express-async-handler');
+const status = require("http-status");
 
 // @description     Form analytics
 // @route           GET /api/form/formAnalytics/:id
 // @access          ADMIN
 const analytics = expressAsyncHandler(async (req, res, next) => {
+
+  const allowedUsers = [
+    "ADMIN",
+    "PRESIDENT",
+    "VICEPRESIDENT",
+    "DIRECTOR_CREATIVE",
+    "DIRECTOR_TECHNICAL",
+    "DIRECTOR_MARKETING",
+    "DIRECTOR_OPERATIONS",
+    "DIRECTOR_SPONSORSHIP"
+  ]
+  if (!allowedUsers.includes(req.user.access) && req.user.email != "srex@fedkiit") {
+    return next(new ApiError(status.UNAUTHORIZED, "Access Denied"))
+  }
+
   try {
     console.log("entering form analytics")
     const { id: formId } = req.params;
     let form = await prisma.form.findUnique({
       where: { id: formId },
-      include: { formAnalytics: true, sections: false, info:true }
+      include: { formAnalytics: true, sections: false, info: true }
     });
-    console.log("form",form);
-    if(form.formAnalytics.length === 0){
-      return next(new ApiError(404, "No users have registered to this form yet"))
+    console.log("form", form);
+    if (form.formAnalytics.length === 0) {
+      return next(new ApiError(status.NOT_FOUND, "No users have registered to this form yet"))
     }
 
     let formAnalytics = form.formAnalytics[0];
     let yearCounts;
-    console.log("form analytics : ",formAnalytics);
+    console.log("form analytics : ", formAnalytics);
     // formAnalytics.regCount = formAnalytics?.regUserEmails?.length;
     try {
       const users = await prisma.user.findMany({
@@ -34,9 +50,9 @@ const analytics = expressAsyncHandler(async (req, res, next) => {
 
       });
       yearCounts = users.reduce((acc, obj) => {
-      acc[obj.year] = (acc[obj.year] || 0) + 1;
-      return acc;
-    }, {});
+        acc[obj.year] = (acc[obj.year] || 0) + 1;
+        return acc;
+      }, {});
     } catch (error) {
       console.error("Error fetching all the users form the array list", error);
       // next(new ApiError(500, "Internal Server Error", error));
@@ -46,7 +62,7 @@ const analytics = expressAsyncHandler(async (req, res, next) => {
     return res.status(200).json({ message: "success", form: form, yearCounts });
   } catch (error) {
     console.log(error)
-    next(new ApiError(500, "Internal Server Error", error));
+    next(new ApiError(status.INTERNAL_SERVER_ERROR, "Internal Server Error", error));
   }
 });
 
