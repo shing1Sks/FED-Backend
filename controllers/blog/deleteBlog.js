a4865ac751d58e3ae2d33c5d748fda4500490598
Rudrika <rudrika08.panigrahi@gmail.com>
2025-05-10 14:41:05 +0530

    getblog

--- controllers/blog/deleteBlog.js
+++ controllers/blog/deleteBlog.js
@@ -1,1 +1,36 @@
-// write code to delete a blog post
+const { ApiError } = require("../../utils/error/ApiError");
+const { PrismaClient } = require("@prisma/client");
+const status = require("http-status");
+const deleteImage = require("../../utils/image/deleteImage");
+
+const prisma = new PrismaClient();
+
+const deleteBlog = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+
+        // Check if the blog exists
+        const blog = await prisma.blog.findUnique({
+            where: { id: Number(id) },
+        });
+
+        if (!blog) {
+            return next(new ApiError(status.NOT_FOUND, "Blog not found"));
+        }
+
+        // Delete the image associated with the blog
+          await deleteImage(blog.image, "BLOG");
+
+        // Delete the blog
+        await prisma.blog.delete({
+            where: { id: Number(id) },
+        });
+
+        res.status(status.OK).json({ message: "Blog deleted successfully" });
+    } catch (error) {
+        console.error("Error deleting blog:", error);
+        next(new ApiError(status.INTERNAL_SERVER_ERROR, "An error occurred while deleting the blog"));
+    }
+};
+
+module.exports = { deleteBlog };
